2daa52866f7c8f2ad343a13f22859339ecd843d6
rivka <rivkamiller@hilma.tech>
2024-11-20 14:34:56 +0200

    add login html

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function App({ setCurrentUser, currentUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin() {
+    alert("entered handle login function")
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Welcome to our docs !</h1>
+      <h1>Login Here to see the light</h1>
+      <label htmlFor="username">Username:</label>
+      <br></br>
+      <input
+        placeholder="tzofia"
+        type="text"
+        className="username"
+        value={username}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
+      />
+      <br></br>
+      <br></br>
+      <label htmlFor="password">PassWord:</label>
+      <br></br>
+      <input
+        placeholder="123456"
+        type="text"
+        className="password"
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <br></br>
+      <br></br>
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
 }
 
 export default App
+
+
