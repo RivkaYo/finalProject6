f7749a575499e6ca4ee59739ca5ce65e863e55c2
tzofiaturgeman <tzofiaturgeman@hilma.tech>
2024-11-20 19:54:06 +0200

    final folder

--- client/src/components/Folder.jsx
+++ client/src/components/Folder.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import DeleteBtn from "./DeleteBtn";
 import RenameBtn from "./RenameBtn";
 
-const Folder = () => {
+const Folder = (props) => {
   return (
-    <div>
-      <h1>This is a folder</h1>
+    <div style={{backgroundColor:"whitesmoke",margin:"5px",padding:"5px",border:"2px,solid,black"}}>
+      {/* <h1>This is a folder</h1> */}
+      <h2>{props.name}</h2>
       <br />
       <RenameBtn />
       <DeleteBtn />
